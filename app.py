78136ccd35bcc7414a9dfd83791d1710d6803ba5
bbennett80 <seek4learning@gmail.com>
2021-12-04 00:34:32 -0800

    expand risk factors for models in app

--- app.py
+++ app.py
@@ -1,95 +1,64 @@
 # https://bcrisktool.cancer.gov/about.html
+# https://ems-trials.org/riskevaluator/documents/Breast_cancer_prediction_model.pdf
 
 import streamlit as st
 import streamlit.components.v1 as components
 
+import risk_factors as risk
+
 
 st.title('Genetic Cancer Risk Assessment')
 
+
+# pedigree
+def draw_pedigree_chart():
+    with open('./src/index.html') as p:
+        pedigree = p.read()
+
+    return components.html(pedigree, height=500)
+
+
 # sidebar elements
-model = st.sidebar.selectbox('Risk Model', ('BOADICEA','Tyrer-Cusick', 'Gain', 'Claus', 'General Population', 'BRCAPro'))
-st.subheader(model)
+models = risk.models()
+pedigree_have_need = risk.pedigree_have_need()
 
-def upload_pedigree():
-    return st.sidebar.file_uploader('Upload Pedigree', type=['PED', 'LINKAGE', 'GEDCOM', 'BOADICEA'])
-
-def menarche_age():
-    return st.sidebar.slider('Age of First Menstruation', min_value=0, max_value=45, step=1, value=14, help='use "0" if never had period')
-
-def parity():
-    return st.sidebar.slider('Number of Children', min_value=0, max_value=20, step=1)
-
-def age_first_live_birth():
-     return st.sidebar.slider('Age at First Live Birth', min_value=16, max_value=50, step=1)
-
-def oral_contraceptive_use():
-    return st.sidebar.selectbox('Have you used oral birth control?', ('No', 'Yes'), help='This refers to birth control pill')
-
-def oral_contraceptive_use_duration():
-    return st.sidebar.number_input('Years of oral birth control use', min_value=0, step=1)
-
-def hrt():
-    return st.sidebar.selectbox('Have you used post-menopause hormone replacement?', ('No', 'Yes'))
-
-def hrt_duration():
-    return st.sidebar.number_input('Years of hormone replacement therapy use', min_value=0, step=1)
-
-def height():
-    return st.sidebar.number_input('Height (inches)', min_value=40, value=60, step=1)
-
-def weight():
-    return st.sidebar.number_input('Weight (pounds)', min_value=50, value=120, step=1)
-
-def bmi(height, weight):
-    return round(703 * (weight/height**2), 1)
-
-def alcohol_intake():
-    return st.sidebar.selectbox('Do you drink alcohol?', ('No', 'Yes'))
-
-def alcohol_intake_quantity():
-    return st.sidebar.number_input('How many "standard drinks" do you have per month?', min_value=0, step=1,
-            help='''In the United States, one "standard drink" (or one alcoholic drink equivalent) contains roughly 14 grams of pure alcohol, which is found in:
-
-            12 ounces of regular beer, which is usually about 5% alcohol
-            5 ounces of wine, which is typically about 12% alcohol
-            1.5 ounces of distilled spirits, which is about 40% alcohol''')
-
-def age_menopause():
-    return st.sidebar.number_input('Age of menopause', min_value=30, value=55, step=1, help='use "0" if not menopausal')
-
-def breast_density():
-    return st.sidebar.number_input('BIRADS score', min_value=0, max_value=4, help='Breast density score/value')
-
-def tubal_ligation():
-    return st.sidebar.selectbox('Have you had a tubal ligation?', ('No', 'Yes'))
-
-def endometriosis():
-    return st.sidebar.selectbox('Have you had endometriosis?', ('No', 'Yes'))
-
-upload_pedigree = upload_pedigree()
-menarche_age = menarche_age()
-parity = parity()
-age_first_live_birth = age_first_live_birth()
-oral_contraceptive_use = oral_contraceptive_use()
-if oral_contraceptive_use == 'Yes':
-    oral_contraceptive_use_duration = oral_contraceptive_use_duration()
-hrt = hrt()
-if hrt == 'Yes':
-    hrt_duration = hrt_duration()
+if pedigree_have_need == 'Choose an option':
+    pass
+elif pedigree_have_need == 'Create new pedigree':
+    draw_pedigree_chart = draw_pedigree_chart()
 else:
-    hrt_duration = 0
-height = height()
-weight = weight()
-bmi = bmi(height, weight)
-alcohol_intake = alcohol_intake()
+    upload_pedigree = risk.upload_pedigree()
+
+menarche_age = risk.menarche_age()
+parity = risk.parity()
+age_first_live_birth = risk.age_first_live_birth()
+oral_contraceptive_use = risk.oral_contraceptive_use()
+
+if oral_contraceptive_use == 'Yes':
+    oral_contraceptive_use_duration = risk.oral_contraceptive_use_duration()
+else:
+    oral_contraceptive_use_duration = 0
+
+hrt = risk.hrt()
+height = risk.height()
+weight = risk.weight()
+bmi = risk.bmi(height, weight)
+alcohol_intake = risk.alcohol_intake()
+
 if alcohol_intake == 'Yes':
-    alcohol_intake_quantity = alcohol_intake_quantity()
+    alcohol_intake_quantity = risk.alcohol_intake_quantity()
 else:
     alcohol_intake_quantity = 0
-age_menopause = age_menopause()
-breast_density = breast_density()
-tubal_ligation = tubal_ligation()
-endometriosis = endometriosis()
+
+age_menopause = risk.age_menopause()
+breast_density = risk.breast_density()
+tubal_ligation = risk.tubal_ligation()
+endometriosis = risk.endometriosis()
+brca_status = risk.brca_status()
+breast_biopsy = risk.breast_biopsy()
+ovarian_cancer = risk.ovarian_cancer()
+ashkenazi_jewish = risk.ashkenazi_jewish()
+
 
 def risk_factors():
     risk_factors = {
@@ -98,33 +67,25 @@ def risk_factors():
         'age_first_live_birth': age_first_live_birth,
         'oral_contraceptive_use': oral_contraceptive_use,
         'oral_contraceptive_use_duration': oral_contraceptive_use_duration,
-        'hrt' :hrt,
-        'hrt_duration': hrt_duration,
+        'hrt': hrt,
         'bmi': bmi,
-        'alcohol_intake' :alcohol_intake,
+        'alcohol_intake': alcohol_intake,
         'alcohol_intake_quantity': alcohol_intake_quantity,
         'age_menopause': age_menopause,
         'breast_density': breast_density,
         'tubal_ligation': tubal_ligation,
-        'endometriosis': endometriosis
+        'endometriosis': endometriosis,
+        'brca_status': brca_status,
+        'breast_biopsy': breast_biopsy,
+        'ovarian_cancer': ovarian_cancer,
+        'ashkenazi_jewish': ashkenazi_jewish
     }
     return risk_factors
 
 
 
-# pedigree
-def pedigree_chart():
-    with open('./src/index.html') as p:
-        pedigree = p.read()
-
-    return pedigree 
-
-st.write(risk_factors())
-#pedigree_chart = pedigree_chart()
-#components.html(pedigree_chart, height=500)
-
-
-def tyrer_cusick(age, height, weight, menarche):
+def tyrer_cusick(age, height, weight, menarche_age, parity, age_menopause, hrt, 
+                brca_status, breast_biopsyi, ovarian_cancer, ashkenazi_jewish):
     pass
 
 def BOADICEA():
@@ -132,34 +93,3 @@ def BOADICEA():
     pass
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def risk_factors():
-    # """idea from https://github.com/CCGE-BOADICEA/bws"""
-    # menarche_age = st.sidebar.slider('Age of First Menstruation', min_value=0, max_value=45, step=1, value=14, help='use "0" if never had period')
-    # parity = st.sidebar.slider('Number of Children', min_value=0, max_value=20, step=1)
-    # age_first_live_birth = st.sidebar.slider('Age of First Live Birth', 16, 50, 1)
-    # oral_contraceptive_use = st.sidebar.selectbox('Have you used oral birth control?', ('Yes', 'No'), help='This refers to birth control pill')
-    # hrt = st.sidebar.selectbox('Have you used post-menopause hormone replacement?', ('Yes', 'No'))
-
-    # height = st.sidebar.number_input('Height (inches)', min_value=40, value=60, step=1)
-    # weight = st.sidebar.number_input('Weight (pounds)', min_value=50, value=120, step=1)
-    # bmi = round(703 * (weight/height**2), 1)
-
-    # alcohol_intake = st.sidebar.selectbox('Do you drink alcohol?', ('Yes', 'No'))
-    # age_menopause = st.sidebar.number_input('Age of menopause', min_value=30, value=55, step=1, help='use "0" if not menopausal')
-    # breast_density = st.sidebar.number_input('BIRADS score', min_value=0, max_value=4, help='Breast density score/value')
-    # tubal_ligation = st.sidebar.selectbox('Have you had a tubal ligation?', ('Yes', 'No'))
-    # endometriosis = st.sidebar.selectbox('Have you had endometriosis?', ('Yes', 'No'))
